074f76b41cba772d500226dcd4650b5abca1fc0e
Lazy <161107455+Lazy-Jjk@users.noreply.github.com>
2025-04-23 17:11:53 +0530

    Update BankDemo.java

--- BankDemo.java
+++ BankDemo.java
@@ -1,84 +1,60 @@
 abstract class BankAccount {
-    protected double balance;
-    protected String accountNumber;
-    
-    public BankAccount(String accountNumber, double initialBalance) {
-        this.accountNumber = accountNumber;
-        this.balance = initialBalance;
+    double balance;
+    String accountNumber;
+
+    BankAccount(String accNum, double bal) {
+        accountNumber = accNum;
+        balance = bal;
     }
-    
+
     abstract void deposit(double amount);
     abstract void withdraw(double amount);
-    
-    public void displayBalance() {
-        System.out.println("Account Number: " + accountNumber);
-        System.out.println("Current Balance: $" + balance);
-    }
 }
 
 class SavingsAccount extends BankAccount {
-    private double interestRate;
-    
-    public SavingsAccount(String accountNumber, double initialBalance, double interestRate) {
-        super(accountNumber, initialBalance);
-        this.interestRate = interestRate;
+    SavingsAccount(String accNum, double bal) {
+        super(accNum, bal);
     }
-    
-    @Override
+
     void deposit(double amount) {
         balance += amount;
-        System.out.println("Deposited $" + amount + " to savings account");
+        System.out.println("Savings: Deposited $" + amount);
     }
-    
-    @Override
+
     void withdraw(double amount) {
         if (amount <= balance) {
             balance -= amount;
-            System.out.println("Withdrawn $" + amount + " from savings account");
-        } else {
-            System.out.println("Insufficient funds");
-        }
+            System.out.println("Savings: Withdrew $" + amount);
+        } else System.out.println("Savings: Insufficient funds");
     }
 }
 
 class CurrentAccount extends BankAccount {
-    private double overdraftLimit;
-    
-    public CurrentAccount(String accountNumber, double initialBalance, double overdraftLimit) {
-        super(accountNumber, initialBalance);
-        this.overdraftLimit = overdraftLimit;
+    double overdraftLimit = 500;
+
+    CurrentAccount(String accNum, double bal) {
+        super(accNum, bal);
     }
-    
-    @Override
+
     void deposit(double amount) {
         balance += amount;
-        System.out.println("Deposited $" + amount + " to current account");
+        System.out.println("Current: Deposited $" + amount);
     }
-    
-    @Override
+
     void withdraw(double amount) {
-        if (amount <= (balance + overdraftLimit)) {
+        if (amount <= balance + overdraftLimit) {
             balance -= amount;
-            System.out.println("Withdrawn $" + amount + " from current account");
-        } else {
-            System.out.println("Exceeds overdraft limit");
-        }
+            System.out.println("Current: Withdrew $" + amount);
+        } else System.out.println("Current: Exceeds overdraft limit");
     }
 }
 
 public class BankDemo {
     public static void main(String[] args) {
-        SavingsAccount savings = new SavingsAccount("SA001", 1000, 0.05);
-        CurrentAccount current = new CurrentAccount("CA001", 2000, 500);
-        
-        System.out.println("Savings Account:");
-        savings.deposit(500);
-        savings.withdraw(200);
-        savings.displayBalance();
-        
-        System.out.println("\nCurrent Account:");
-        current.deposit(1000);
-        current.withdraw(3000);
-        current.displayBalance();
+        BankAccount sa = new SavingsAccount("SA001", 1000);
+        BankAccount ca = new CurrentAccount("CA001", 1500);
+
+        sa.deposit(200); sa.withdraw(300);
+        ca.deposit(500); ca.withdraw(2200);
     }
-} 
+}
