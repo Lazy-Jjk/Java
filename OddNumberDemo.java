af94f56f552654de589e284ba8fe47d07d4ea4e5
Lazy <161107455+Lazy-Jjk@users.noreply.github.com>
2025-04-24 08:52:05 +0530

    Update OddNumberDemo.java

--- OddNumberDemo.java
+++ OddNumberDemo.java
@@ -1,23 +1,16 @@
-class OddNumberException extends Exception {
-    public OddNumberException(String message) {
-        super(message);
+public class OddNumberExceptionDemo {
+    static void checkEven(int num) throws Exception {
+        if (num % 2 != 0)
+            throw new Exception("Number is odd!");
+        else
+            System.out.println("Number is even.");
     }
-}
 
-public class OddNumberDemo {
-    public static void checkEvenNumber(int number) throws OddNumberException {
-        if (number % 2 != 0) {
-            throw new OddNumberException(number + " is an odd number!");
-        }
-        System.out.println(number + " is an even number.");
-    }
-    
     public static void main(String[] args) {
         try {
-            checkEvenNumber(4);
-            checkEvenNumber(7);
-        } catch (OddNumberException e) {
-            System.out.println("Error: " + e.getMessage());
+            checkEven(5); // Change to test other numbers
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
         }
     }
-} 
+}
